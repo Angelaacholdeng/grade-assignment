639df5f9ca02878fc9f373f314d2f1e827ea217c
Angelaacholdeng <dengacholangela@gmail.com>
2021-03-24 08:37:29 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function getgrade(marks){
+    if(marks>=80){ 
+        console.log("grade  A");
+}
+    else if(marks>=70){ 
+        console.log("grade  B");}
+        else if(marks>=60){
+            console.log("grade  C");
+        }
+        else if(marks>=50){
+            console.log("grade  D");
+        }else 
+        console.log("grade  F")
+}
+console.log(getgrade(64))
